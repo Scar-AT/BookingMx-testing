5ca9e35738f774c3ae3fd0976d01a168e85e3980
Scarlett AT <tsuki.scarlett@gmail.com>
2025-11-11 23:01:48 -0600

    chore: setup and verify Junit and JaCoCo environment

---
+++ backend/src/test/java/com/bookingmx/reservations/service/ReservationServiceTest.java
@@ -0,0 +1,13 @@
+package com.bookingmx.reservations.service;
+
+import org.junit.jupiter.api.Test;
+
+public class ReservationServiceTest {
+
+    @Test
+    void contextLoads() {
+        System.out.println("JUNIT IS WORKING!!");
+    }
+
+
+}
